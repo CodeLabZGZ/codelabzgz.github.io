a2375734c16440cef6e7b32f2d597dafb2b5100f
Hector Toral <amra@visual.htb>
2024-03-16 19:37:29 +0100

    feat: login form

--- src/app/(app)/sign-in/page.tsx
+++ src/app/(app)/sign-in/page.tsx
@@ -1,9 +1,96 @@
-import { SignInButton } from "@clerk/nextjs";
+import { StarField } from '@/components/star-field'
 
-export default function page() {
+export default function NotFound() {
   return (
-    <div>
-      <SignInButton />
+    <div className="w-full relative isolate flex flex-auto flex-col items-center justify-center overflow-hidden bg-gray-950 text-center">
+      <svg
+        aria-hidden="true"
+        className="absolute left-1/2 top-[-10vh] -z-10 h-[120vh] w-[120vw] min-w-[60rem] -translate-x-1/2"
+      >
+        <defs>
+          <radialGradient id="gradient" cy="0%">
+            <stop offset="0%" stopColor="rgba(56, 189, 248, 0.3)" />
+            <stop offset="53.95%" stopColor="rgba(0, 71, 255, 0.09)" />
+            <stop offset="100%" stopColor="rgba(10, 14, 23, 0)" />
+          </radialGradient>
+        </defs>
+        <rect width="100%" height="100%" fill="url(#gradient)" />
+      </svg>
+      <StarField className="sm:-mt-16" />
+
+      <div className="w-full flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+            Sign in to your account
+          </h2>
+          <p className="mt-2 text-sm leading-6 text-gray-200">
+            Not a member?{' '}
+            <a href="/#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+              Start a 14 day free trial
+            </a>
+          </p>
+        </div>
+
+        <div className="mt-10 mx-auto max-w-sm w-full">
+          <div className="bg-white px-6 py-10 shadow sm:rounded-md">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium text-left leading-6 text-gray-900">
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder='codelabzgz@unizar.es'
+                    autoComplete="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password" className="block text-sm font-medium text-left leading-6 text-gray-900">
+                    Password
+                  </label>
+                  <div className="text-sm">
+                    <a href="/#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                      Forgot password?
+                    </a>
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder='****************'
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
