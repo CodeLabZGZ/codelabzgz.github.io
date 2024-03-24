a4884b6aeef7df24ec830f1243c1b2c606145d5c
Hector Toral <amra@visual.htb>
2024-03-24 16:28:14 +0100

    feat: pocketbase removed

--- src/app/(app)/teams/all.jsx
+++ src/app/(app)/teams/all.jsx
@@ -1,10 +1,4 @@
-import pb from "@/lib/pocketbase"
-
 export default async function All () {
-  await pb.collection("teams").getFullList()
-    .then(records => console.log(records))
-    .catch(err => console.error(err))
-
   return (
     <div>Todos</div>
   )
--- src/lib/pocketbase.js
+++
@@ -1,6 +0,0 @@
-import PocketBase from "pocketbase"
-
-const pb = new PocketBase(process.env.PB_API)
-await pb.admins.authWithPassword(process.env.PB_USER, process.env.PB_PASS)
-
-export default pb
