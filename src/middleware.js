7fc47fea84c7b4888ed9e2c95945f56a48d87190
Hector Toral <amra@visual.htb>
2024-03-29 16:16:27 +0100

    feat: auth app

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,27 @@
 import NextAuth from "next-auth"
+import { NextResponse } from "next/server"
 import authConfig from "./auth.config"
-export const { auth: middleware } = NextAuth(authConfig)
+
+export const { auth } = NextAuth(authConfig)
+
+const protectedRoutes = [
+  "/events",
+  "/teams",
+  "/profile"
+]
+
+export default async function middleware (request) {
+  const session = await auth()
+
+  const { pathname, origin } = request.nextUrl
+
+  const isProtectedRoute = protectedRoutes.some((prefix) => pathname.startsWith(prefix))
+
+  if (!session && isProtectedRoute) {
+    const absoluteURL = new URL("/", origin)
+    return NextResponse.redirect(absoluteURL.toString())
+  }
+}
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
