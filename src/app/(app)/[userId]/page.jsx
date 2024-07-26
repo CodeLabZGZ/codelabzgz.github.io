fbc9556d459376c7e9b3403b8f3f1a0e7c34a8eb
Hector Toral <hectortoraltrabajos@gmail.com>
2024-07-27 00:42:51 +0200

    logica del perfil

--- src/app/(app)/[userId]/page.jsx
+++ src/app/(app)/[userId]/page.jsx
@@ -18,53 +18,31 @@ import {
 } from "@/components/ui/table"
 import { db } from "@/db"
 import { users } from "@/schema"
-import { eq } from "drizzle-orm"
+import { eq, sql } from "drizzle-orm"
 import { notFound } from "next/navigation"
 
-const scoreboards = [
-  {
-    event: "INV001",
-    challenge: "Paid",
-    date: "2024-05-01",
-    points: 50
-  },
-  {
-    event: "INV002",
-    challenge: "Pending",
-    date: "2024-05-01",
-    points: 20
-  },
-  {
-    event: "INV003",
-    challenge: "Unpaid",
-    date: "2024-05-01",
-    points: 30
-  },
-  {
-    event: "INV004",
-    challenge: "Paid",
-    date: "2024-05-01",
-    points: 25
-  },
-  {
-    event: "INV005",
-    challenge: "Paid",
-    date: "2024-05-01",
-    points: 25
-  },
-  {
-    event: "INV006",
-    challenge: "Pending",
-    date: "2024-05-01",
-    points: 30
-  },
-  {
-    event: "INV007",
-    challenge: "Unpaid",
-    date: "2024-05-01",
-    points: 40
-  }
-]
+function groupByType(events) {
+  return events.reduce((acc, event) => {
+    const { type } = event
+
+    if (!acc[type]) acc[type] = 0
+    acc[type] += 1
+
+    return acc
+  }, {})
+}
+
+function groupByMonthAndSumPoints(events) {
+  return events.reduce((acc, event) => {
+    const date = new Date(event.timestamp)
+    const month = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}`
+
+    if (!acc[month]) acc[month] = 0
+    acc[month] += event.points
+
+    return acc
+  }, {})
+}
 
 export default async function Page({ params: { userId } }) {
   const user = await db.query.users.findFirst({
@@ -79,6 +57,44 @@ export default async function Page({ params: { userId } }) {
   })
 
   if (!user) return notFound()
+
+  const totalChallenges = db.all(sql`
+    SELECT difficulty, COUNT(*) as count
+    FROM challenges
+    GROUP BY difficulty;
+  `)
+  console.log(totalChallenges)
+
+  const __scoreboards = db.all(sql`
+    SELECT e.title as event, c.title as challenge, c.points, c.difficulty, e.type, sc2.timestamp
+    FROM (
+      SELECT sc.event, sc.challenge, sc.user, MAX(sc.points) AS points2
+      FROM scoreboards sc
+      WHERE user = 'e19f7678-a990-4595-8ee3-a1faac3d5963'
+      GROUP BY sc.event, sc.challenge, sc.user
+    ) d
+    INNER JOIN events e ON e.id = d.event
+    INNER JOIN challenges c ON c.title = d.challenge
+    INNER JOIN scoreboards sc2 ON sc2.event = d.event and sc2.challenge = d.challenge and sc2.user = d.user and d.points2 = sc2.points
+    ORDER BY sc2.timestamp DESC;
+  `)
+  console.log(__scoreboards)
+
+  const groupedByType = Object.entries(groupByType(__scoreboards)).map(
+    ([type, total]) => ({
+      type,
+      total
+    })
+  )
+  console.log(groupedByType)
+
+  const groupedByMonth = Object.entries(
+    groupByMonthAndSumPoints(__scoreboards)
+  ).map(([month, totalPoints]) => ({
+    month,
+    totalPoints
+  }))
+  console.log(groupedByMonth)
 
   const opciones = { day: "numeric", month: "short", year: "numeric" }
   const formatter = new Intl.DateTimeFormat("es-ES", opciones)
@@ -106,7 +122,9 @@ export default async function Page({ params: { userId } }) {
         </Card>
         <Card className="w-fit min-w-44 rounded">
           <CardHeader>
-            <CardTitle>813</CardTitle>
+            <CardTitle>
+              {__scoreboards.reduce((prev, curr) => prev + curr.points, 0)}
+            </CardTitle>
             <CardDescription>Puntuación total</CardDescription>
           </CardHeader>
         </Card>
@@ -137,15 +155,15 @@ export default async function Page({ params: { userId } }) {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {scoreboards.map(scoreboard => (
+            {__scoreboards.map(scoreboard => (
               <TableRow key={scoreboard.invoice}>
                 <TableCell className="font-medium">
                   {scoreboard.event}
                 </TableCell>
                 <TableCell>{scoreboard.challenge}</TableCell>
                 <TableCell>
-                  <time dateTime={scoreboard.date} className="capitalize">
-                    {formatter.format(new Date(scoreboard.date))}
+                  <time dateTime={scoreboard.timestamp} className="capitalize">
+                    {formatter.format(new Date(scoreboard.timestamp))}
                   </time>
                 </TableCell>
                 <TableCell className="text-right font-mono text-xs">
