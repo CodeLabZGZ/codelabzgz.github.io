58e01e7e2c7e9796dc79d4ad83f6e34808009c53
Hector Toral <hectortoraltrabajos@gmail.com>
2024-07-29 12:12:43 +0200

    scoreboard temas

--- db/functions/teams/get-scoreboard.js
+++ db/functions/teams/get-scoreboard.js
@@ -1,26 +1,34 @@
 import { db } from "@/db"
-import { challenges, scoreboards, teams } from "@/schemas"
-import { desc, eq, sql, sum } from "drizzle-orm"
+import { sql } from "drizzle-orm"
 
 export const getScoreboard = async ({ limit, offset }) => {
-  const data = await db
-    .select({
-      rank: sql`ROW_NUMBER() OVER (ORDER BY ${sum(challenges.points)} DESC, ${teams.createdAt} ASC)`,
-      team: {
-        slug: teams.slug,
-        image: teams.image,
-        name: teams.name,
-        motto: teams.motto
-      },
-      points: sum(challenges.points)
-    })
-    .from(scoreboards)
-    .innerJoin(challenges, eq(scoreboards.challenge, challenges.title))
-    .innerJoin(teams, eq(scoreboards.team, teams.slug))
-    .groupBy(scoreboards.event, scoreboards.challenge, scoreboards.team)
-    .orderBy(desc(sum(challenges.points)), teams.createdAt)
-    .limit(limit)
-    .offset(offset)
+  const data = db
+    .all(
+      sql`
+    -- PUNTOS MAXIMOS DE UN EQUIPO EN EL EVENTO 1 / CHALLENGE - (INCLUYE FECHA)
+    SELECT t.slug AS slug, t.image, t.name, r.cpoints as points
+    FROM (
+      SELECT MT.team, SUM(c.points) AS cpoints, SUM(MT.points) AS spoints
+      FROM (
+      -- PUNTOS MAXIMOS DE UN EQUIPO EN EL EVENTO 1 / CHALLENGE - (NO INCLUYE FECHA)
+        SELECT sc.event, sc.challenge, sc.team, MAX(sc.points) AS points
+        FROM scoreboards sc
+        WHERE sc.team NOT NULL
+        GROUP BY sc.event, sc.challenge, sc.team
+      ) MT
+      INNER JOIN scoreboards lb ON lb.event = MT.event AND lb.challenge = MT.challenge AND lb.team = MT.team AND lb.points = MT.points
+      INNER JOIN challenges c ON c.title = MT.challenge
+      GROUP BY MT.team
+    ) r
+    INNER JOIN teams t ON t.slug = r.team
+    ORDER BY r.cpoints DESC, r.spoints DESC
+  `
+    )
+    .map(({ points, ...team }, rank) => ({
+      rank: rank + 1,
+      team: { ...team },
+      points
+    }))
 
   return data
 }
