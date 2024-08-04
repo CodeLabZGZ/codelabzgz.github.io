1f1013b1c9c35ca7115616b2dcd17ab63f9c908d
Hector Toral <hectortoraltrabajos@gmail.com>
2024-08-04 23:03:18 +0200

    bug import

--- src/app/api/v1/teams/scoreboard/route.js
+++ src/app/api/v1/teams/scoreboard/route.js
@@ -1,6 +1,7 @@
 import { getScoreboard } from "@/functions/teams/get-scoreboard"
 import { response } from "@/lib/utils"
 import { errorHandler } from "@/middlewares/error-handler"
+import { auth } from "@/auth"
 
 async function getHandler() {
   const data = await getScoreboard()
